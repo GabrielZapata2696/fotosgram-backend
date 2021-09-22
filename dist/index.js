31b4007e95e650b447e36a08a630d71e06bffc2b
Gabriel Zapata <sr.gabrielzm@gmail.com>
2021-09-22 15:32:34 -0500

    Adicion rutas para los servicios de usuario e importacion del mismo en index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('Hola Gabriel!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = __importDefault(require("./classes/server"));
+const usuarioRoutes_1 = __importDefault(require("./routes/usuarioRoutes"));
+const server = new server_1.default;
+//rutas de la aplicación
+server.app.use('/user', usuarioRoutes_1.default);
+//levantar servidor express
+server.start(() => {
+    console.log(`Servidor levantado en puerto: ${server.port}`);
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('Hola Gabriel!')
+import Server from './classes/server';
+import userRoutes from './routes/usuarioRoutes';
+
+const server = new Server;
+
+//rutas de la aplicación
+server.app.use('/user', userRoutes);
+
+
+//levantar servidor express
+server.start(() => {
+    console.log(`Servidor levantado en puerto: ${server.port}`)
+});
